7b82df59e893cb5680736e92c91ccc5393c13047
eminakcil <eminakcil@outlook.com>
2022-12-04 23:16:47 +0300

    seed user on first run

--- src/loaders/db.js
+++ src/loaders/db.js
@@ -1,9 +1,13 @@
 import Mongoose from 'mongoose'
+import User from '../models/User'
 
 const db = Mongoose.connection
 
 db.once('open', () => {
   console.log('Connected to db!')
+
+  // uncommet bottom line if you want a quick login
+  // seedUser()
 })
 
 const connectDB = async () => {
@@ -14,6 +18,25 @@ const connectDB = async () => {
       useUnifiedTopology: true,
     }
   )
+}
+
+// eslint-disable-next-line no-unused-vars
+const seedUser = async () => {
+  await User.deleteMany({})
+  console.log('deleted all users')
+
+  const user = {
+    name: 'John Doe',
+    email: 'johndoe@domain.com',
+    password: '1234567',
+  }
+
+  await User.create(user)
+  console.log('your admin info')
+  console.log(user)
+
+  db.close()
+  process.exit()
 }
 
 export { connectDB }
